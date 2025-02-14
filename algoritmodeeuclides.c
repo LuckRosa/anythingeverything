49c2fd9cd4489c7f3eff2d464eeee26da0115a99
LuckRosa <lrosad00@estudiantes.unileon.es>
2025-02-14 16:55:53 +0100

    algoritmo de euclides en c sin compilar

---
+++ algoritmodeeuclides.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+	int D, d, c, r;
+	printf("ALGORÍTMO DE EUCLIDES \n Introduzca dos números: \n");
+	scanf("%d", &D);
+	c = D/d;
+	r = D%d;
+	while(r != 0){
+		D = d;
+		d = r;
+		c = D/d;
+		r = D%d;
+	}
+	printf("El mcd de %d y %d es", D, d);
+
+return 0;
+}
