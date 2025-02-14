03a2874249f111113c07547a40e051bfd04792b3
LuckRosa <lrosad00@estudiantes.unileon.es>
2025-02-14 17:34:28 +0100

    añadidas nuevas funciones
    Maneja el caso en el que el mcd sale 1, es decir, cuando no existe. También lanza ahora un mensaje secreto (pista: después de introducir los dos números).

--- algoritmodeeuclides.c
+++ algoritmodeeuclides.c
@@ -8,13 +8,25 @@ int main (){
 	dorig = d;
 	c = D/d;
 	r = D%d;
+
+	if (Dorig < dorig){
+		printf("Eres un cretino \n");
+	}
+
 	while(r != 0){
 		D = d;
 		d = r;
 		c = D/d;
 		r = D%d;
 	}
-	printf("El mcd de %d y %d es %d \n", Dorig, dorig, d);
 
+	if (d==1){
+		printf("No existe un mcd entre %d y %d", Dorig, dorig);
+	}
+	else{
+		printf("El mcd de %d y %d es %d \n", Dorig, dorig, d);
+	}
+
+	
 return 0;
 }
