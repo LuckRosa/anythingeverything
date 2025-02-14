e265e3382e98aabacdee797447128d1a3450c85c
LuckRosa <lrosad00@estudiantes.unileon.es>
2025-02-14 17:18:21 +0100

    arreglos codigo

--- algoritmodeeuclides.c
+++ algoritmodeeuclides.c
@@ -4,6 +4,8 @@ int main (){
 	printf("ALGORÍTMO DE EUCLIDES \n Introduzca dos números: \n");
 	scanf("%d", &D);
 	scanf("%d", &d);
+	Dorig = D;
+	dorig = d;
 	c = D/d;
 	r = D%d;
 	while(r != 0){
@@ -12,7 +14,7 @@ int main (){
 		c = D/d;
 		r = D%d;
 	}
-	printf("El mcd de %d y %d es", D, d);
+	printf("El mcd de %d y %d es %d \n", Dorig, dorig, d);
 
 return 0;
 }
